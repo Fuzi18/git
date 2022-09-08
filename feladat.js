7b0124307fed183ecf2a18e1f951e17e624be49e
Füzesi Tamás <fuzesi.t2003@gmail.com>
2022-09-08 12:49:32 +0200

    urururfzf

---
+++ feladat.js
@@ -0,0 +1,7 @@
+let tomb = [];
+for (let i = 0; i < 10; i++) {
+  tomb[i]=Math.floor(Math.random()*100)
+
+}
+tomb = tomb.sort();
+console.log(tomb[i]);
---
+++ macska.js
@@ -0,0 +1,7 @@
+let macskat = []
+for (let i = 0; i < 20; i++){
+  macskat[i] = {
+    
+    eletkor: Math.round(2 + Math.random() * 8)
+  }
+}
